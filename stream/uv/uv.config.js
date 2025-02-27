36185ac318b1ddde541f23b985b0f6476e274ead
Jake Bliss <65978090+acool1036@users.noreply.github.com>
2025-02-27 03:57:59 -0600

    Update uv.config.js

--- stream/uv/uv.config.js
+++ stream/uv/uv.config.js
@@ -1,13 +1,13 @@
 self.__uv$config = {
-    prefix: '/embed/',
+    prefix: 'embed/',
     
     // BARE SERVER
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'https://sysadmin.us.kg',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: 'uv.handler.js',
+    bundle: 'uv.bundle.js',
+    config: 'uv.config.js',
+    sw: 'uv.sw.js',
 };
