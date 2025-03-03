fd14c350876bf2f5628f6065a3704e13d871c9aa
Jake Bliss <65978090+acool1036@users.noreply.github.com>
2025-03-03 06:11:46 -0600

    Update uv.config.js

--- stream/uv/uv.config.js
+++ stream/uv/uv.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
-    prefix: '/Stream-Star/stream/embed/movie/',
+    prefix: '/Stream-Star/stream/embed/watch/',
     
     // BARE SERVER
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    bare:'https:https://sysadmin.us.kg/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
