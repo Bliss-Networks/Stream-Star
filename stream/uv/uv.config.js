ee179d78d6ab95a95a0f33eb6680c9711fb6c6fe
Jake Bliss <65978090+acool1036@users.noreply.github.com>
2025-03-02 21:31:24 -0600

    Update uv.config.js

--- stream/uv/uv.config.js
+++ stream/uv/uv.config.js
@@ -1,13 +1,13 @@
 self.__uv$config = {
-    prefix: '/embed/',
+    prefix: '/Stream-Star/stream/',
     
     // BARE SERVER
     bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: '/Stream-Star/stream/uv/uv.handler.js',
+    bundle: '/Stream-Star/stream/uv/uv.bundle.js',
+    config: '/Stream-Star/stream/uv/uv.config.js',
+    sw: '/Stream-Star/stream/uv/uv.sw.js',
 };
